6844248d09ca882ff534d4d6a6bc5c78470ac495
ma <m.fuica01@ufromail.cl>
2021-12-04 22:33:35 -0300

    chore(docs): añade datos de presentacion

--- src/main/java/com/ufrontera/java/Controller/Main.java
+++ src/main/java/com/ufrontera/java/Controller/Main.java
@@ -3,7 +3,9 @@ package com.ufrontera.java.Controller;
 import com.ufrontera.java.Model.*;
 
 /**
- * apellido1-nombre1_apellido2-nombre2
+ * Diccionario Interactivo
+ * Complejidad Computacional
+ * Universidad de La Frontera
  *
  * [2021-12-04 Sat]
  * Authores: godoi-misael_hijerra-camilo_sepulveda-jose_fuica-morales-manuel
@@ -12,6 +14,7 @@ import com.ufrontera.java.Model.*;
  *           Sepulveda Jose
  *           Fuica Morales Manuel
  */
+
 public class Main {
     public static void main( String[] args ) {
         String hline = "---------------------------------";
