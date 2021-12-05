fc59d0282dd22dba92f8e16901f78acc90243de3
ma <m.fuica01@ufromail.cl>
2021-12-04 21:46:50 -0300

    chore: autores, menu demo

--- src/main/java/com/ufrontera/java/Controller/App.java
+++ src/main/java/com/ufrontera/java/Controller/App.java
@@ -1,13 +1,30 @@
-package com.ufrontera.java;
+package com.ufrontera.java.Controller;
 
 /**
- * Hello world!
+ * apellido1-nombre1_apellido2-nombre2
  *
+ * [2021-12-04 Sat]
+ * Authores: godoi-misael_hijerra-camilo_sepulveda-jose_fuica-morales-manuel
+ *           Godoi Misael
+ *           Hijerra Camilo
+ *           Sepulveda Jose
+ *           Fuica Morales Manuel
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String hline = "-------------------------------------------------------------------------------------------------------------------------------------";
+        System.out.println("Complejidad Computacional");
+        System.out.println("Proyecto Final");
+        System.out.println(hline);
+        System.out.println(hline);
+
+        System.out.println("Mostrar palabras");
+        System.out.println("Agregar palabra");
+        System.out.println("Buscar palabra");
+        System.out.println("Eliminar palabra");
+        System.out.println("Mostrar palabras");
+
     }
 }
