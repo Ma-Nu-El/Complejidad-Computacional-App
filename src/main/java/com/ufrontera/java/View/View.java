df45bd8afb55f641d337b1e4bfd2554c1f96ed00
ma <m.fuica01@ufromail.cl>
2021-12-15 15:38:40 -0300

    style(tabs): Replace tabs with spaces
    Also delete unused methods.

--- src/main/java/com/ufrontera/java/View/View.java
+++ src/main/java/com/ufrontera/java/View/View.java
@@ -10,104 +10,63 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class View extends JFrame{
+public class View extends JFrame {
 
-	private JTextField firstNumber  = new JTextField(10);
-	private JLabel additionLabel = new JLabel("+");
-	private JTextField secondNumber = new JTextField(10);
-	private JButton calculateButton = new JButton("Calculate");
-	private JTextField calcSolution = new JTextField(10);
     // Importar
     // Exportar
     // Mostrar
     // Agregar
     // Eliminar
     // Buscar
-	private JButton importButton = new JButton("Importar");
-	private JButton exportButton = new JButton("Exportar");
-	private JButton displayButton = new JButton("Mostrar");
-	private JButton addButton = new JButton("Agregar");
-	private JButton deleteButton = new JButton("Borrar");
-	private JButton searchButton = new JButton("Buscar");
+    private JButton importButton = new JButton("Importar");
+    private JButton exportButton = new JButton("Exportar");
+    private JButton displayButton = new JButton("Mostrar");
+    private JButton addButton = new JButton("Agregar");
+    private JButton deleteButton = new JButton("Borrar");
+    private JButton searchButton = new JButton("Buscar");
 
-    public View(){
+    public View() {
 
-		// Sets up the view and adds the components
+        // Sets up the view and adds the components
 
-		JPanel calcPanel = new JPanel();
+        JPanel calcPanel = new JPanel();
 
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(600, 200);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 200);
 
-		calcPanel.add(firstNumber);
-		calcPanel.add(additionLabel);
-		calcPanel.add(secondNumber);
-		calcPanel.add(calculateButton);
-		calcPanel.add(calcSolution);
+        calcPanel.add(importButton);
+        calcPanel.add(exportButton);
+        calcPanel.add(displayButton);
+        calcPanel.add(addButton);
+        calcPanel.add(deleteButton);
+        calcPanel.add(searchButton);
 
+        this.add(calcPanel);
 
-		calcPanel.add(importButton);
-		calcPanel.add(exportButton);
-		calcPanel.add(displayButton);
-		calcPanel.add(addButton);
-		calcPanel.add(deleteButton);
-		calcPanel.add(searchButton);
+        // End of setting up the components --------
 
-		this.add(calcPanel);
-
-		// End of setting up the components --------
-
-	}
-
-	public int getFirstNumber(){
-
-		return Integer.parseInt(firstNumber.getText());
-
-	}
-
-	public int getSecondNumber(){
-
-		return Integer.parseInt(secondNumber.getText());
-
-	}
-
-	public int getCalcSolution(){
-
-		return Integer.parseInt(calcSolution.getText());
-
-	}
-
-	public void setCalcSolution(int solution){
-
-		calcSolution.setText(Integer.toString(solution));
-
-	}
-
-	// If the calculateButton is clicked execute a method
-	// in the Controller named actionPerformed
+    }
 
     // Listener para boton 'Importar'
-	public void addImportListener(ActionListener listenForCalcButton){
+    public void addImportListener(ActionListener listenForCalcButton) {
 
-		importButton.addActionListener(listenForCalcButton);
+        importButton.addActionListener(listenForCalcButton);
 
-	}
-
+    }
 
     // Listener para boton 'Exportar'
-	public void addExportListener(ActionListener listenForCalcButton){
+    public void addExportListener(ActionListener listenForCalcButton) {
 
-		exportButton.addActionListener(listenForCalcButton);
+        exportButton.addActionListener(listenForCalcButton);
 
-	}
+    }
 
+    // Open a popup that contains the error message passed
 
-	// Open a popup that contains the error message passed
+    public void displayErrorMessage(String errorMessage) {
 
-	public void displayErrorMessage(String errorMessage){
+        JOptionPane.showMessageDialog(this, errorMessage);
 
-		JOptionPane.showMessageDialog(this, errorMessage);
-
-	}
+    }
 
 }
