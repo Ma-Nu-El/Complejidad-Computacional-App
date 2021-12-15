a2bc65bb80a9ef7565465d513f6671a59d113fd3
ma <m.fuica01@ufromail.cl>
2021-12-15 16:51:54 -0300

    refactor(vista): Poda

--- src/main/java/com/ufrontera/java/View/View.java
+++ src/main/java/com/ufrontera/java/View/View.java
@@ -1,5 +1,7 @@
 package com.ufrontera.java.View;
 
+import java.awt.BorderLayout;
+
 // This is the View
 // Its only job is to display what the user sees
 // It performs no calculations, but instead passes
@@ -12,75 +14,67 @@ import javax.swing.*;
 
 public class View extends JFrame {
 
-    // Importar
-    // Exportar
-    // Mostrar
-    // Agregar
-    // Eliminar
-    // Buscar
+    private JPanel panel = new JPanel();
+
     private JButton importButton = new JButton("Importar");
     private JButton exportButton = new JButton("Exportar");
     private JButton displayButton = new JButton("Mostrar");
     private JButton addButton = new JButton("Agregar");
     private JButton deleteButton = new JButton("Borrar");
     private JButton searchButton = new JButton("Buscar");
+    private JButton closeButton = new JButton("Cerrar");
 
     public View() {
 
-        // Sets up the view and adds the components
-
-        JPanel appPanel = new JPanel();
-
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1000, 700);
+        this.setTitle("UFRO Diccionario");
+        this.add(panel);
 
-        appPanel.add(importButton);
-        appPanel.add(exportButton);
-        appPanel.add(displayButton);
-        appPanel.add(addButton);
-        appPanel.add(deleteButton);
-        appPanel.add(searchButton);
-
-        this.add(appPanel);
-
-        // End of setting up the components --------
+        // Botones
+        panel.add(importButton);
+        panel.add(exportButton);
+        panel.add(displayButton);
+        panel.add(addButton);
+        panel.add(searchButton);
+        panel.add(deleteButton);
+        panel.add(closeButton);
 
     }
 
-
     // LISTENERS
 
-    // Listener para boton 'Importar'
+    // Boton 'Importar'
     public void addImportListener(ActionListener listenForImportButton) {
 
         importButton.addActionListener(listenForImportButton);
 
     }
 
-    // Listener para boton 'Exportar'
+    // Boton 'Exportar'
     public void addExportListener(ActionListener listenForExportButton) {
 
         exportButton.addActionListener(listenForExportButton);
 
     }
 
-    // Listener para boton 'Mostrar'
+    // Boton 'Mostrar'
     public void addDisplayListener(ActionListener listenForDisplayButton){
         displayButton.addActionListener(listenForDisplayButton);
     }
 
-    // Listener para boton 'Agregar'
+    // Boton 'Agregar'
     public void addAddListener(ActionListener listenForAddButton){
         addButton.addActionListener(listenForAddButton);
 
     }
 
-    // Listener para boton 'Borrar'
+    // Boton 'Borrar'
     public void addDeleteListener(ActionListener listenForDeleteButton){
         deleteButton.addActionListener(listenForDeleteButton);
     }
 
-    // Listener para boton 'Buscar'
+    // Boton 'Buscar'
     public void addSearchListener(ActionListener listenForSearchButton){
         searchButton.addActionListener(listenForSearchButton);
     }
